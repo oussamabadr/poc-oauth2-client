83594da3d4fe8d65e66a6af4c8e09dd7eb93979d
oussamabadr <oussamabadr@gmail.com>
2022-02-22 17:40:47 +0100

    first commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    void shouldAnswerWithTrue() {
+        assertThat(true).isTrue();
+    }
+}
